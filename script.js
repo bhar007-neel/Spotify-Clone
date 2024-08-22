1678354b887c802cf403a589e4cd41a66f73d46a
Neelmani Bhrdwaj <neelmanibhardwaj001@gmail.com>
2024-08-22 04:57:02 -0400

    adding more changes, adding js to our project

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("Let write Javascript")
+let currentSong = new Audio()
+let isPlaying = false;
 
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 async function getSongs() {
 
     let a = await fetch("http://127.0.0.1:3000/songs/")
@@ -19,38 +34,111 @@ async function getSongs() {
     }
     return songs
 }
+
+const playMusic = (track,pause= false) => {
+    // If a different track is selected, stop the current one
+    if(!pause){
+        currentSong.play()
+    }
+    if (currentSong.src !== "/songs/" + track) {
+        currentSong.pause(); // Ensure any currently playing audio is stopped
+        currentSong.src = "/songs/" + track;
+    }
+
+    // Attempt to play the current song
+    currentSong.play().then(() => {
+        play.src = "pause.svg";
+        isPlaying = true;
+    }).catch(error => {
+        console.error("Audio play failed:", error);
+    });
+
+    document.querySelector('.songinfo').innerHTML= track
+    document.querySelector('.songtime').innerHTML="00:00/00:00"
+}
+
 async function main() {
+    // let currentSong = new Audio();
     // get the ist of all songs
     let songs = await getSongs()
-    console.log(songs)
+    playMusic(songs[0],true)
 
-   let songUL= document.querySelector(".songlist").getElementsByTagName("ul")[0]
-   for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `  <li>
+
+
+    // Show all th songs in playlist
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `  <li>
                             <img class="invert" src="music.svg" alt="">
                             <div class="info">
-                                <div>${song}</div>
+                                <div>${song.replaceAll("%20", "")}</div>
                                 <div>Neel</div>
                             </div>
                             <div class="playnow">
                                 <span>Playnow</span>
                                 <img class="invert" src="play-button-svgrepo-com (1).svg" alt="">
                             </div>
-                        </li> `
-    
-   }
+                        </li> `;
 
-    // play the first audio
-    var audio = new Audio(songs[0]);
-    audio.play();
+    }
 
-
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(audio.duration,audio.currentSrc,audio.currentTime)
-        // The duration variable now holds the duration (in seconds) of the audio clip
+    //    attach a event listener to each song
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        });
     });
-     
+
+    // // play the first audio
+    // var audio = new Audio(songs[0]);
+    // audio.play();
+
+
+    // audio.addEventListener("loadeddata", () => {
+    //     let duration = audio.duration;
+    //     console.log(audio.duration,audio.currentSrc,audio.currentTime)
+    //     // The duration variable now holds the duration (in seconds) of the audio clip
+    // });
+
+    //  Attach an event listener to next and previous
+    play.addEventListener("click", () => {
+        if (isPlaying) {
+            currentSong.pause();
+            play.src = "play-button-svgrepo-com (1).svg";
+            isPlaying = false;
+        } else {
+            currentSong.play().then(() => {
+                play.src = "pause.svg";
+                isPlaying = true;
+            }).catch(error => {
+                console.error("Audio play failed:", error);
+            });
+        }
+    });
+
+    // Listen for time update event
+    currentSong.addEventListener("timeupdate",()=>{
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML =
+        `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left =(currentSong.currentTime/ currentSong.duration)*100
+        +"%"
+    })
+
+      // Add an event listener to seekbar
+      document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+    // add a event listner for eventburger
+
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left ="0"
+    })
+
 }
 
-    main()
+main()
